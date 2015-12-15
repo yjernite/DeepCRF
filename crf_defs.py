fixed parameter and gradient clipping, L2 regularized

crf_defs.py
@@ -33,8 +33,8 @@ def potentials_layer(in_layer, mask, config, params, reuse=False, name='Potentia
     else:
         W_pot = weight_variable([input_size, pot_card], name=name)
         b_pot = bias_variable([pot_card], name=name)
-        W_pot = tf.clip_by_norm(W_pot, 5)
-        b_pot = tf.clip_by_norm(b_pot, 5)
+        W_pot = tf.clip_by_norm(W_pot, config.param_clip)
+        b_pot = tf.clip_by_norm(b_pot, config.param_clip)
     flat_input = tf.reshape(in_layer, [-1, input_size])
     pre_scores = tf.matmul(flat_input, W_pot) + b_pot
     pots_layer = tf.reshape(pre_scores, out_shape)
@@ -223,14 +223,20 @@ class CRF:
     
     def make(self, config, params, reuse=False, name='CRF'):
         with tf.variable_scope(name):
-            ### EMBEDDING
+            self.l1_norm = tf.reduce_sum(tf.zeros([1]))
+            self.l2_norm = tf.reduce_sum(tf.zeros([1]))
+            ### EMBEDDING LAYER
             if reuse:
                 tf.get_variable_scope().reuse_variables()
-            # out_layer <- output of NN (TODO: add layers)
+            # initial embedding
             (out_layer, embeddings) = feature_layer(self.input_ids,
                                                     config, params,
                                                     reuse=reuse)
             params.embeddings = embeddings
+            for feat in config.l1_list:
+                self.l1_norm += L1_norm(params.embeddings[feat])
+            for feat in config.l2_list:
+                self.l2_norm += L2_norm(params.embeddings[feat])
             if config.verbose:
                 print('features layer done')
             # convolution
@@ -239,26 +245,33 @@ class CRF:
                                                           params, reuse=reuse)
                 params.W_conv = W_conv
                 params.b_conv = b_conv
+                # self.l1_norm += L1_norm(W_conv) + L1_norm(b_conv)
+                self.l2_norm += L2_norm(W_conv) + L2_norm(b_conv)
                 if config.verbose:
                     print('convolution layer done')
             self.out_layer = out_layer
-            ### SequNN
-            (preds_layer, W_pred, b_pred) = predict_layer(out_layer, config,
-                                                          params, reuse=reuse)
-            params.W_pred = W_pred
-            params.b_pred = b_pred
-            self.preds_layer = preds_layer
-            (cross_entropy, accu_nn) = optim_outputs(preds_layer,
-                                                      self.nn_targets,
-                                                      config, params)
+            ### SEQU-NN
+            if config.nn_obj_weight > 0:
+                (preds_layer, W_pred, b_pred) = predict_layer(out_layer, config,
+                                                              params, reuse=reuse)
+                params.W_pred = W_pred
+                params.b_pred = b_pred
+                # self.l1_norm += L1_norm(W_pred) + L1_norm(b_pred)
+                self.l2_norm += L2_norm(W_pred) + L2_norm(b_pred)
+                self.preds_layer = preds_layer
+                (cross_entropy, accu_nn) = optim_outputs(preds_layer,
+                                                          self.nn_targets,
+                                                          config, params)
             ### CRF
-            # pots_layer <- potentials
+            # potentials
             (pots_layer, W_pot, b_pot) = potentials_layer(out_layer,
                                                           self.mask,
                                                           config, params,
                                                           reuse=reuse)
             params.W_pot = W_pot
             params.b_pot = b_pot
+            # self.l1_norm += L1_norm(W_pot) + L1_norm(b_pot)
+            self.l2_norm += L2_norm(W_pot) + L2_norm(b_pot)
             if config.verbose:
                 print('potentials layer done')
             self.pots_layer = pots_layer
@@ -273,25 +286,24 @@ class CRF:
             cond_accuracy = tf.reduce_sum(correct_cond_pred * tf.reduce_sum(self.targets, 2)) /\
                             tf.reduce_sum(self.targets)
             self.cond_accuracy = cond_accuracy
-            # log-likelihood
+            # log-likelihood, tensor to list
             pots_list = tf.split(0, config.batch_size, self.pots_layer)
             pots_list = [tf.squeeze(pots) for pots in pots_list]
             tags_list = tf.split(0, config.batch_size, self.tags)
             tags_list = [tf.squeeze(tags) for tags in tags_list]
             args_list = zip(pots_list, tags_list)
-            self.args_list = args_list      # useless -> DEBUG
+            # log-likelihood, dynamic programming
             dynamic = [tf.user_ops.chain_sum_product(pots, tags)
                        for pots, tags in args_list]
-            self.dynamic = dynamic          # useless -> DEBUG
             pre_crf_list = [(pots, tags, f_sp, b_sp, grads)
                             for ((pots, tags), (f_sp, b_sp, grads)) in zip(args_list, dynamic)]
             crf_list = [tf.user_ops.chain_crf(pots, tags, f_sp, b_sp, grads)
                         for (pots, tags, f_sp, b_sp, grads) in pre_crf_list]
+            # log-likelihood, compute
             log_likelihoods = tf.pack([ll for (ll, marg) in crf_list])
-            self.log_likelihoods = log_likelihoods    # useless -> DEBUG
-            self.marginals = tf.pack([marg for (ll, marg) in crf_list])
             log_likelihood = tf.reduce_sum(log_likelihoods)
             self.log_likelihood = log_likelihood
+            self.marginals = tf.pack([marg for (ll, marg) in crf_list])
             # map assignment and accuracy of map assignment
             map_tags = map_assignment(self.pots_layer, config)
             correct_pred = tf.equal(map_tags, tf.argmax(self.targets, 2))
@@ -301,25 +313,15 @@ class CRF:
             self.map_tags = map_tags
             self.accuracy = accuracy
             ### OPTIMIZATION
-            # L1 regularization
-            self.l1_norm = tf.reduce_sum(tf.zeros([1]))
-            for feat in config.l1_list:
-                self.l1_norm += config.l1_reg * \
-                                tf.reduce_sum(tf.abs(params.embeddings[feat]))
-            # L2 regularization
-            self.l2_norm = tf.reduce_sum(tf.zeros([1]))
-            for feat in config.l2_list:
-                self.l2_norm += config.l2_reg * \
-                                tf.reduce_sum(tf.mul(params.embeddings[feat],
-                                                     params.embeddings[feat]))
             # different criteria
             self.criteria = {}
             self.criteria['pseudo_ll'] = -self.pseudo_ll
             self.criteria['likelihood'] = -self.log_likelihood
-            norm_penalty = config.l1_reg * self.l1_norm + config.l1_reg * self.l2_norm
+            norm_penalty = config.l1_reg * self.l1_norm + config.l2_reg * self.l2_norm
             for k in self.criteria:
-                self.criteria[k] = (1 - config.nn_obj_weight) * self.criteria[k] + \
-                                   norm_penalty + config.nn_obj_weight * cross_entropy
+                self.criteria[k] += norm_penalty
+                if config.nn_obj_weight > 0:
+                    self.criteria[k] -= (config.nn_obj_weight * cross_entropy)
             # corresponding training steps, gradient clipping
             optimizers = {}
             for k in self.criteria:
@@ -330,10 +332,10 @@ class CRF:
                     optimizers[k] = tf.train.AdamOptimizer(config.learning_rate,
                                                               name='adam_' + k)
             grads_and_vars = {}
+            # gradient clipping
             for k, crit in self.criteria.items():
                 uncapped_g_v = optimizers[k].compute_gradients(crit,
                                                                tf.trainable_variables())
-                # print config.gradient_clip
                 grads_and_vars[k] = [(tf.clip_by_norm(g, config.gradient_clip), v) if g else (g, v)
                                      for g, v in uncapped_g_v]
             self.train_steps = {}
@@ -358,8 +360,10 @@ class CRF:
             total_crit += crit
             if i % 50 == 0:
                 train_accuracy = self.accuracy.eval(feed_dict=f_dict)
-                print("step %d of %d, training accuracy %f, criterion %f" %
-                      (i, n_batches, train_accuracy, crit))
+                print("step %d of %d, training accuracy %f, criterion %f, ll %f, l1 %f, l2 %f" %
+                      (i, n_batches, train_accuracy, crit,
+                       self.log_likelihood.eval(feed_dict=f_dict),
+                       self.l1_norm.eval(), self.l2_norm.eval()))
         print 'total crit', total_crit / n_batches
         return total_crit / n_batches
     

crf_training.py
@@ -38,32 +38,23 @@ sess = tf.InteractiveSession()
 
 
 ### log-likelihood
-config.learning_rate = 1e-2
-config.l1_reg = 1e-2
+config.learning_rate = 1e-3
 config.l2_list = config.input_features
-config.l2_reg = 1e-2
-config.nn_obj_weight = 0
 
 config.gradient_clip = 1
-
-config.use_convo = True
-config.features_dim = 200
-config.conv_dim = 200
-
-config.optimizer = 'adam'
-# config.optimizer = 'adagrad'
-# config.criterion = 'pseudo_ll'
+config.param_clip = 25
 
 crf = CRF(config)
 crf.make(config, params_crf)
+
 
 sequ_nn = SequNN(config)
 sequ_nn.make(config, params_nn)
 
 sess.run(tf.initialize_all_variables())
 
-accuracies_nn, preds_nn = train_model(train_data, dev_data, crf, config, params_crf, 'sequ_nn')
-accuracies_crf, preds_crf = train_model(train_data, dev_data, crf, config, params_nn, 'CRF')
+accuracies_nn, preds_nn = train_model(train_data, dev_data, sequ_nn, config, params_nn, 'sequ_nn')
+accuracies_crf, preds_crf = train_model(train_data, dev_data, crf, config, params_crf, 'CRF')
 
 
 crf_predictions = [fuse_preds_crf(sent, pred, config)
@@ -98,11 +89,3 @@ if True:
         evaluate(merged, 0.1 * i)
 
 #~ execfile('crf_training.py')
-
-#~ for i in range(10):
-    #~ print 'epoch ----------------', i
-    #~ shuffle(train_data_32)
-    #~ crf.train_epoch(train_data_32, config, params, sess, crit_type='pseudo_ll')
-    #~ crf.validate_accuracy(train_data_32, config)
-    #~ crf.validate_accuracy(dev_data_32, config)
-

nn_defs.py
@@ -104,8 +104,8 @@ def convo_layer(in_layer, config, params, reuse=False, name='Convo'):
         W_conv = weight_variable([conv_window, 1, input_size, output_size],
                                  name=name)
         b_conv = bias_variable([output_size], name=name)
-        W_conv = tf.clip_by_norm(W_conv, 5)
-        b_conv = tf.clip_by_norm(b_conv, 5)
+        W_conv = tf.clip_by_norm(W_conv, config.param_clip)
+        b_conv = tf.clip_by_norm(b_conv, config.param_clip)
     reshaped = tf.reshape(in_layer, [batch_size, num_steps, 1, input_size])
     conv_layer = tf.nn.relu(tf.reshape(conv2d(reshaped, W_conv),
                                        [batch_size, num_steps, output_size],
@@ -125,8 +125,8 @@ def predict_layer(in_layer, config, params, reuse=False, name='Predict'):
     else:
         W_pred = weight_variable([input_size, n_outcomes], name=name)
         b_pred = bias_variable([n_outcomes], name=name)
-        W_pred = tf.clip_by_norm(W_pred, 5)
-        b_pred = tf.clip_by_norm(b_pred, 5)
+        W_pred = tf.clip_by_norm(W_pred, config.param_clip)
+        b_pred = tf.clip_by_norm(b_pred, config.param_clip)
     flat_input = tf.reshape(in_layer, [-1, input_size])
     pre_scores = tf.nn.softmax(tf.matmul(flat_input, W_pred) + b_pred)
     preds_layer = tf.reshape(pre_scores, [batch_size, num_steps, -1])
@@ -138,16 +138,13 @@ def optim_outputs(outcome, targets, config, params):
     num_steps = int(outcome.get_shape()[1])
     n_outputs = int(outcome.get_shape()[2])
     # We are currently using cross entropy as criterion
-    criterion = -tf.reduce_sum(targets * tf.log(outcome))
-    for feat in config.l1_list:
-        criterion += config.l1_reg * \
-                     tf.reduce_sum(tf.abs(params.embeddings[feat]))
+    cross_entropy = tf.reduce_sum(targets * tf.log(outcome))
     # We also compute the per-tag accuracy
     correct_prediction = tf.equal(tf.argmax(outcome, 2), tf.argmax(targets, 2))
     accuracy = tf.reduce_sum(tf.cast(correct_prediction,
                                      "float") * tf.reduce_sum(targets, 2)) /\
         tf.reduce_sum(targets)
-    return (criterion, accuracy)
+    return (cross_entropy, accuracy)
 
 
 class SequNN:
@@ -166,11 +163,17 @@ class SequNN:
     
     def make(self, config, params, reuse=False, name='SequNN'):
         with tf.variable_scope(name):
+            self.l1_norm = tf.reduce_sum(tf.zeros([1]))
+            self.l2_norm = tf.reduce_sum(tf.zeros([1]))
             if reuse:
                 tf.get_variable_scope().reuse_variables()
             (out_layer, embeddings) = feature_layer(self.input_ids, config,
                                                     params, reuse=reuse)
             params.embeddings = embeddings
+            for feat in config.l1_list:
+                self.l1_norm += L1_norm(params.embeddings[feat])
+            for feat in config.l2_list:
+                self.l2_norm += L2_norm(params.embeddings[feat])
             if config.verbose:
                 print('features layer done')
             if config.use_rnn:
@@ -182,6 +185,8 @@ class SequNN:
                                                           params, reuse=reuse)
                 params.W_conv = W_conv
                 params.b_conv = b_conv
+                # self.l1_norm += L1_norm(W_conv) + L1_norm(b_conv)
+                self.l2_norm += L2_norm(W_conv) + L2_norm(b_conv)
                 if config.verbose:
                     print('convolution layer done')
             self.out_layer = out_layer
@@ -189,24 +194,15 @@ class SequNN:
                                                           params, reuse=reuse)
             params.W_pred = W_pred
             params.b_pred = b_pred
+            # self.l1_norm += L1_norm(W_pred) + L1_norm(b_pred)
+            self.l2_norm += L2_norm(W_pred) + L2_norm(b_pred)
             self.preds_layer = preds_layer
             (cross_entropy, accuracy) = optim_outputs(preds_layer, self.targets, config, params)
             if config.verbose:
                 print('output layer done')
             self.accuracy = accuracy
-            # L1 regularization
-            self.l1_norm = tf.reduce_sum(tf.zeros([1]))
-            for feat in config.l1_list:
-                self.l1_norm += config.l1_reg * \
-                                tf.reduce_sum(tf.abs(params.embeddings[feat]))
-            # L2 regularization
-            self.l2_norm = tf.reduce_sum(tf.zeros([1]))
-            for feat in config.l2_list:
-                self.l2_norm += config.l2_reg * \
-                                tf.reduce_sum(tf.mul(params.embeddings[feat],
-                                                     params.embeddings[feat]))
-            norm_penalty = config.l1_reg * self.l1_norm + config.l1_reg * self.l2_norm
-            criterion = cross_entropy + norm_penalty
+            norm_penalty = config.l1_reg * self.l1_norm + config.l2_reg * self.l2_norm
+            criterion = -cross_entropy + norm_penalty
             self.criterion = criterion
             # optimization
             if config.optimizer == 'adagrad':
@@ -230,9 +226,9 @@ class SequNN:
                       self.targets: batch.tag_windows_one_hot}
             if i % 100 == 0:
                 train_accuracy = self.accuracy.eval(feed_dict=f_dict)
-                print("step %d of %d, training accuracy %f, Lemma_l1 %f" %
+                print("step %d of %d, training accuracy %f, l1 %f, l2 %f" %
                       (i, len(data) / batch_size, train_accuracy,
-                       tf.reduce_sum(tf.abs(params.embeddings['lemma'])).eval()))
+                       self.l1_norm.eval(), self.l2_norm.eval()))
             self.train_step.run(feed_dict=f_dict)
     
     def validate_accuracy(self, data, config):

utils.py
@@ -1,6 +1,7 @@
 # A few utility functions
 import itertools
 import numpy as np
+import tensorflow as tf
 from pprint import pprint
 
 
@@ -23,13 +24,14 @@ def linearize_indices(indices, dims):
 ###############################################
 class Config:
     def __init__(self, batch_size=20, num_steps=32, learning_rate=1e-2,
-                 l1_reg=2e-3, l1_list=[],
-                 l2_reg=2e-3, l2_list=[],
-                 nn_obj_weight=0.1,
-                 optimizer='adagrad', gradient_clip=1e1, criterion='likelihood',
-                 features_dim=50, init_words=False, input_features=[],
+                 l1_reg=1e-2, l1_list=[],
+                 l2_reg=1e-2, l2_list=[],
+                 nn_obj_weight=-1,
+                 optimizer='adam', criterion='likelihood',
+                 gradient_clip=1e0, param_clip=1e2,
+                 features_dim=200, init_words=False, input_features=[],
                  use_rnn=False, rnn_hidden_units=100, rnn_output_size=50,
-                 use_convo=False, conv_window=5, conv_dim=50,
+                 use_convo=True, conv_window=5, conv_dim=200,
                  pot_size=1,
                  pred_window=1, tag_list=[],
                  verbose=False, num_epochs=10, num_predict=5):
@@ -44,9 +46,10 @@ class Config:
         self.l2_list = l2_list
         self.nn_obj_weight = nn_obj_weight
         # optimization configuration
-        self.optimizer = optimizer
+        self.optimizer = optimizer          # ['adam', 'adagrad']
+        self.criterion = criterion          # ['likelihood', 'pseudo_ll']
         self.gradient_clip = gradient_clip
-        self.criterion = criterion
+        self.param_clip = param_clip
         # input layer
         self.features_dim = features_dim
         self.init_words = init_words
@@ -290,6 +293,15 @@ def cut_batches(data, config):
            for i in range(num_cuts)]
     res[-1] = res[-1] + [pad_token] * (config.num_steps - len(res[-1]))
     return res
+
+
+# norm functions
+def L1_norm(tensor):
+    return tf.reduce_sum(tf.abs(tensor))
+
+
+def L2_norm(tensor):
+    return tf.reduce_sum(tf.mul(tensor, tensor))
 
 
 ###############################################