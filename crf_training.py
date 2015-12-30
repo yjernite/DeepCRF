310983fc94ae34edfe391025c18653555c9015e4
Yacine Jernite <jernite@snappy3.cims.nyu.edu>
2015-12-30 10:35:10 -0500

    .

--- crf_training.py
+++ crf_training.py
@@ -27,22 +27,34 @@ def main():
     crf = CRF(config)
     crf.make(config, params_crf)
     sess.run(tf.initialize_all_variables())
-    # train
-    accuracies_crf, preds_crf = train_model(train_data, dev_data, crf, config, params_crf, 'CRF')
-    # print results: accuracies
-    print '##### Parameters'
-    pprint(config.to_string().splitlines())
-    print '##### Train/dev accuracies: NN'
-    pprint(accuracies_nn)
-    print '##### Train/dev accuracies: CRF'
-    pprint(accuracies_crf)
-    # print results: F measures
-    for ep in range(config.num_predict, config.num_epochs + 1, config.num_predict):
-        print '---------- epoch', ep
-        #~ crf_predictions = [fuse_preds_crf(sent, pred, config)
-                           #~ for sent, pred in zip(dev_data, preds_crf[ep])]
-        sentences = preds_to_sentences(preds_crf[ep], config)
+    train_data_32 = cut_and_pad(train_data, config)
+    for i in range(100):
+        shuffle(train_data_32)
+        print 'training', i
+        crf.train_epoch(train_data_32, config, params_crf)
+        print 'tagging', i
+        preds = tag_dataset(dev_data, config, params_crf, 'CRF')
+        sentences = preds_to_sentences(preds, config)
+        print 'epoch', i
         evaluate(sentences, 0.5)
+
+
+    #~ # train
+    #~ accuracies_crf, preds_crf = train_model(train_data, dev_data, crf, config, params_crf, 'CRF')
+    #~ # print results: accuracies
+    #~ print '##### Parameters'
+    #~ pprint(config.to_string().splitlines())
+    #~ print '##### Train/dev accuracies: NN'
+    #~ pprint(accuracies_nn)
+    #~ print '##### Train/dev accuracies: CRF'
+    #~ pprint(accuracies_crf)
+    #~ # print results: F measures
+    #~ for ep in range(config.num_predict, config.num_epochs + 1, config.num_predict):
+        #~ print '---------- epoch', ep
+        #~ # crf_predictions = [fuse_preds_crf(sent, pred, config)
+                           #~ # for sent, pred in zip(dev_data, preds_crf[ep])]
+        #~ sentences = preds_to_sentences(preds_crf[ep], config)
+        #~ evaluate(sentences, 0.5)
 
 
 if __name__ == "__main__":
