b7146d20f394be3e81e566d83b9b8df833d039f9
yacine <yacine.jernite@nyu.edu>
2016-01-01 13:24:47 -0800

    fix

--- crf_defs.py
+++ crf_defs.py
@@ -337,7 +337,7 @@ class CRF:
             #~ self.l2_norm += L2_norm(W_pot) + L2_norm(b_pot)
             (bin_pots, W_p_b, b_p_b) = binary_log_pots(out_layer, config,
                                                        params, reuse=reuse)
-            params.W_bot_bin = W_p_b
+            params.W_pot_bin = W_p_b
             params.b_pot_bin = b_p_b
             print W_p_b
             print params.W_pot_bin
